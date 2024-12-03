8788170d656080d2dd392353829f33d502876bb5
hernan527 <hernan527@gmail.com>
2024-12-03 14:09:45 -0300

    url server

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,4 +1,3 @@
 
-// export const SERVER_URL = 'http://localhost:5200';
 export const SERVER_URL = 'https://cotizador.tuchat.com.ar';
 
--- src/app/data/constants/mock/index.ts
+++ src/app/data/constants/mock/index.ts
@@ -338,5 +338,5 @@ export const CREDIT_DATA_ITEMS: Credit[] = [
 	},
 ];
 
-// export const SERVER_URL = 'https://clinicas-listado.onrender.com';
-export const SERVER_URL = 'http://localhost:5200';
+// export const SERVER_URL = 'https://cotizador.tuchat.com.ar';
+// export const SERVER_URL = 'http://localhost:5200';
